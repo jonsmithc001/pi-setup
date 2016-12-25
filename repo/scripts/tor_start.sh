6438ce4d20cc4bd637bf085c16693ff77d19001d
jonsmithc001 <jonathansmithc001@gmail.com>
2016-12-25 19:29:47 +0530

    Update tor_start.sh

--- repo/scripts/tor_start.sh
+++ repo/scripts/tor_start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo bash
+
 sudo iptables -F
 sudo iptables -t nat -F
 
@@ -9,7 +10,4 @@ sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 22 -j REDIRECT --to-p
 sudo iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-ports 53
 sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
 
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-
+sh internet_access.sh
