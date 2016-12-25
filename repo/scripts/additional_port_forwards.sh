fe38c808ea89cb8cd7cbbe2fc42a86ed4fe3f02c
jonsmithc001 <jonathansmithc001@gmail.com>
2016-12-25 20:09:58 +0530

    Update additional_port_forwards.sh

--- repo/scripts/additional_port_forwards.sh
+++ repo/scripts/additional_port_forwards.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-iptables -t nat -A PREROUTING -p tcp -m tcp --dport 9091 -j DNAT --to-destination 192.160.1.18
-iptables -t nat -A POSTROUTING -d 192.160.1.18 -p tcp -m tcp --dport 9091 -j MASQUERADE
-	
+
+for (( c=1000; c<=1010; c++ ))
+do  
+   iptables -t nat -A PREROUTING -p tcp -m tcp --dport $c -j DNAT --to-destination 192.160.1.18
+   iptables -t nat -A POSTROUTING -d 192.160.1.18 -p tcp -m tcp --dport $c -j MASQUERADE 
+done
