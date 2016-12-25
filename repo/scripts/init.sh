e2f23238ec95cac2a25b8c59b2c2204724e33730
jonsmithc001 <jonathansmithc001@gmail.com>
2016-12-25 19:50:41 +0530

    Update init.sh

--- repo/scripts/init.sh
+++ repo/scripts/init.sh
@@ -4,17 +4,9 @@ sudo bash
 sudo iptables -F
 sudo iptables -t nat -F
 
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+sh internet_access.sh
 
-sh additional_port_forwards.sh
+sh ap_restart.sh
 
-
-ap_restart.sh
-
-
-
-
-wget -q -O /dev/null  http://freedns.afraid.org/dynamic/update.php?UnVFQ0RoQ29VNG9hVmRKZnp1NER6a0o1OjE2NTE5NjE3
+sh updateDNS.sh
 
